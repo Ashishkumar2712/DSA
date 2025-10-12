d939ae759a22b125b1882b386f8eff6da1ce1fa7
Ashishkumar2712 <ashish4singh27@gmail.com>
2025-10-13 00:08:40 +0530

    Time: 4 ms (70.27%) | Memory: 45 MB (23.30%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int romanToInt(String s) {
+        
+   Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
+
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int current = roman.get(s.charAt(i));
+
+            if (current < prevValue) {
+                total -= current;
+            } else {
+                total += current;
+            }
+            prevValue = current;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.romanToInt("III"));      // Output: 3
+        System.out.println(sol.romanToInt("LVIII"));    // Output: 58
+        System.out.println(sol.romanToInt("MCMXCIV"));  // Output: 1994
+    }
+}
