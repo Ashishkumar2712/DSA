a74651888347c0de582b9bfea82a22fe98af8c4b
Ashishkumar2712 <ashish4singh27@gmail.com>
2026-01-07 00:24:12 +0530

    Time: 1 ms (85.28%) | Memory: 43.7 MB (6.47%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -2,17 +2,18 @@ class Solution {
     public int numDecodings(String s) {
         int n = s.length();
         int[] dp = new int[n+1];
-        dp[n] = 1 ;
-        for (int i=n-1; i>=0; i--){
-            if (s.charAt(i) != '0'){
-                dp[i] = dp[i+1];
-                if (i<n-1 && (s.charAt(i)=='1' || s.charAt(i)=='2' && s.charAt(i+1)<'7')) {
-                    dp[i] += dp[i+2];
-                }
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= n; i++) {
+            int first = Integer.valueOf(s.substring(i - 1, i));
+            int second = Integer.valueOf(s.substring(i - 2, i));
+            if (first >= 1 && first <= 9) {
+               dp[i] += dp[i-1];  
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i-2];
             }
         }
-
-            
-        return dp[0];
+        return dp[n];
     }
 }
