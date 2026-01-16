19873f8e80e93dcfa66d33a70266720c37f0d686
Ashishkumar2712 <ashish4singh27@gmail.com>
2026-01-16 15:26:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (52.44%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,6 +1,6 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] grid = new int[m] [n];
+     /*   int[][] grid = new int[m] [n];
 
         for (int i=0; i<m; i++){
             for (int j=0; j<n; j++){
@@ -15,5 +15,23 @@ class Solution {
             }
         }
         return grid[m - 1][n - 1];
+    }
+}*/ if(m == 1 || n == 1)
+            return 1;
+        m--;
+        n--;
+        if(m < n) {              // Swap, so that m is the bigger number
+            m = m + n;
+            n = m - n;
+            m = m - n;
+        }
+        long res = 1;
+        int j = 1;
+        for(int i = m+1; i <= m+n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
+            res *= i;
+            res /= j;
+        }
+            
+        return (int)res;
     }
 }
