f90043f17b7609603e5157d4931f6385a868c078
Ashishkumar2712 <ashish4singh27@gmail.com>
2026-01-31 23:34:33 +0530

    Time: 1 ms (46.00%) | Memory: 46.7 MB (12.25%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.java
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new LinkedList<>();
+        dfs(root, list);
+        return list;
+    }
+
+    private void dfs(Node node, List<Integer> list) {
+        if (node == null) return;
+
+        list.add(node.val);
+
+        if (node.children == null) return;
+
+        for (Node child : node.children) {
+            dfs(child, list);
+        }
+    }
+}
