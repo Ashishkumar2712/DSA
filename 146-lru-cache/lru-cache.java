f425689cf4d74f0cf768543ac423c564ff2c19d9
Ashishkumar2712 <ashish4singh27@gmail.com>
2026-02-15 17:20:51 +0530

    Time: 44 ms (83.83%) | Memory: 131.6 MB (43.45%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,59 @@
+class LRUCache {
+
+    class Node {
+        int key, value;
+        Node prev, next;
+        Node(int k, int v) { key = k; value = v; }
+    }
+
+    private int capacity;
+    private Map<Integer, Node> map;
+    private Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void addToHead(Node node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+        Node node = map.get(key);
+        remove(node);
+        addToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.value = value;
+            remove(node);
+            addToHead(node);
+        } else {
+            Node node = new Node(key, value);
+            map.put(key, node);
+            addToHead(node);
+            if (map.size() > capacity) {
+                Node toRemove = tail.prev;
+                remove(toRemove);
+                map.remove(toRemove.key);
+            }
+        }
+    }
+}
