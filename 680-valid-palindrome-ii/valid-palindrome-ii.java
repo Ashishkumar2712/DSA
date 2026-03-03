fb9fc869bd2c4bb4f9ea6414672bcadff7bc7f5a
Ashishkumar2712 <ashish4singh27@gmail.com>
2026-03-03 13:41:02 +0530

    Time: 4 ms (99.53%) | Memory: 47.4 MB (96.68%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int n = s.length();
+        int i=0;
+        int j=n-1;
+
+        while (i<j){
+            if (s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+
+            }
+            else return ispalin(s,i+1,j) || ispalin(s,i,j-1);
+        }
+        return true ;
+    }
+    public boolean ispalin(String s , int i , int j){
+        while (i<j){
+            if (s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }
+            else {
+                return false;
+            }
+        }
+            return true ;
+        
+        
+    }
+}
+       
