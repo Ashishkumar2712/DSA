5e3b9777b194ac5ca565b0467d67a1b4ef7468fc
Ashishkumar2712 <ashish4singh27@gmail.com>
2026-03-06 00:10:14 +0530

    Time: 553 ms (7.24%) | Memory: 19.7 MB (88.98%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -7,17 +7,17 @@ class Solution:
         return True
 
     def minWindow(self, s: str, t: str) -> str:
-        l1 = [0 for i in range(125)]
-        l2 = [0 for i in range(125)]
+        l1 = [0 for i in range(58)]
+        l2 = [0 for i in range(58)]
         for i in t:
-            l1[ord(i)] = l1[ord(i)] + 1
+            l1[ord(i)-ord('A')] = l1[ord(i)- ord('A')] + 1
         
         i = 0
         q = 0
         mini = 1000000
         ans= ""
         while q < len(s):
-            l2[ord(s[q])] = l2[ord(s[q])] + 1
+            l2[ord(s[q])-ord('A')] = l2[ord(s[q])-ord('A')] + 1
             c = self.comparearr(l1,l2)
             if c:
                 while self.comparearr(l1,l2) and i < len(s):
@@ -25,7 +25,7 @@ class Solution:
                         ans=s[i:q+1]
                     mini = min(q - i + 1, mini)
                     
-                    l2[ord(s[i])] = l2[ord(s[i])] - 1
+                    l2[ord(s[i])-ord('A')] = l2[ord(s[i])-ord('A')] - 1
                     i = i + 1
                     
 
