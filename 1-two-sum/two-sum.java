82afcafa959e9e6de1dc655fa35eea9b17b69529
Ashishkumar2712 <ashish4singh27@gmail.com>
2026-04-21 17:22:58 +0530

    Time: 2 ms (99.27%) | Memory: 46.4 MB (97.38%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,28 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
        int n = nums.length;
-        HashMap <Integer, Integer> map = new HashMap<>();
+        HashMap <Integer, Integer> map = new HashMap<>();// hash map bnna kyuki tc ghat jaayega 
 
-        for(int i=0; i<n; i++){
-            int c = target - nums[i];
+        for(int i=0; i<n; i++){// iterate krr 0 se n tkk 
+            int c = target - nums[i];// maan le agar tere pass 9 target hai and usmain tunne 7 ghata dia firr bhi tujh prr 2 bachenge and dekh 2 map main exist krta hai ki nhi
 
             if (map.containsKey(c)){
-                return new int[] {map.get(c),i};
+                return new int[] {map.get(c),i};// return krr 2,7 ko
             }
-          map.put(nums[i], i);
+          map.put(nums[i], i);// yeh line isliye kyuki hmme index return krna hai naa ki value ;
         }
         return new int[] {};
     }
 }
    
-    /*   for (int i =0; i < n ; i++){
-        for (int j=i+1; j < n ; j++){
-            int sum = nums[i]+nums[j];
-            if (sum== target){
-                return new int[] {i,j};
-            }
-        }
-       }
-       return new int[] {0,0};
-    }
-}*/
