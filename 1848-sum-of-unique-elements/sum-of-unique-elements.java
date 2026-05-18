0f69c03647bfa98025cc2e7534875e32a6276be6
Ashishkumar2712 <ashish4singh27@gmail.com>
2026-05-19 01:38:18 +0530

    Time: 1 ms (68.18%) | Memory: 42.5 MB (98.71%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -20,10 +20,38 @@ HashMap <Integer, Integer> map = new HashMap<>();
 for (int i =0; i<n; i++){
 map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
 }
-       
+ //List<Integer> duplicates = new ArrayList<>();      
  for (int i = 0; i < n; i++) {
 if (map.get(nums[i])==1) sum += nums[i];
 }
     return sum;
 }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
