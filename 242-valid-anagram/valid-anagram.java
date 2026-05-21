c22847b1abd2362f890d71b4c4ba4981cb4bbaac
Ashishkumar2712 <ashish4singh27@gmail.com>
2026-05-22 01:49:28 +0530

    Time: 13 ms (29.59%) | Memory: 46.4 MB (32.54%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,23 +1,48 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false ;
-        
-        HashMap< Character ,Integer> map = new HashMap<>();
+      /*  int n = s.length();
+        int m = t.length();
+        if (n!=m ) return false ;
+        int[] arr = new int[26];
 
-       for (char c : s.toCharArray()){
-        map.put (c, map.getOrDefault(c,0)+1);
-       }
-        for (char c : t.toCharArray()){
-            if (!map.containsKey(c)) return false ;
+       
 
-            map.put(c , map.get(c)-1 );
-            if (map.get (c)==0) {
-                map.remove (c);
+        for ( char ch: s.toCharArray()){
+            arr[ch-97]++;
+        }
+          for ( char ch: t.toCharArray()){
+            arr[ch-97]--;
+
+            if (arr[ch-97]<0){
+                return false ;
             }
 
-        
-       }
-       return map.isEmpty();
+
+        }
+        return true ;
         
     }
+}*/
+
+// for follow up //
+ int n = s.length();
+ int m = t.length();
+
+ if (n!=m) return false ;
+
+ HashMap<Character, Integer> map = new HashMap<>();
+
+ for (char ch: s.toCharArray()){
+    map.put(ch, map.getOrDefault(ch,0)+1);
+ }
+
+ for ( char ch: t.toCharArray()){
+    map.put(ch, map.getOrDefault(ch, 0)-1);
+
+
+    if (map.get(ch) < 0) return false ;
+ }
+ return true ;
+    }
 }
+
